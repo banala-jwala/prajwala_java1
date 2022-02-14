981909ef7e505dfa0aa0b42ca8542b7837882351
banala-jwala <banalaprajwala@gmail.com>
2022-02-14 16:57:16 +0530

    my first commit

---
+++ variable.java
@@ -0,0 +1,21 @@
+/*write a java program to compare 2 variables check for equality
+a=5
+b=6
+*/
+
+class variables
+{
+public static void main(String args[])
+{
+int a=5;
+int b=6;
+if(a==b)
+{
+System.out.println("a equal to b");
+}
+else if(a!=b)
+{
+System.out.println("a is not equal to b");
+}
+}
+}
