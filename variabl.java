27547718a9910567c562e70bda4bdcacb013d0c6
banala-jwala <banalaprajwala@gmail.com>
2022-02-15 13:58:35 +0530

    my first commit

---
+++ variabl.java
@@ -0,0 +1,11 @@
+class variabl
+{
+public static void main(String args[])
+{
+int a=4,b=5;
+if(a==b)
+System.out.println("a is equal to b");
+else if(a!=b)
+System.out.println("a is not equal to b");
+}
+}
